222dc6b96313de209d636ee23097125b89e9cf2b
root <root@DESKTOP-FIEJLUR>
2025-01-26 05:15:12 +0900

    Usesテーブル変更

--- authentication/models.py
+++ authentication/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils.timezone import now
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
+# Create your models here.
+
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
