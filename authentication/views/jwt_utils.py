830946cbb44f88aa9bf6a0802aab77810a95c0a2
ARISA1115 <a.engine0808@gmail.com>
2025-02-09 20:00:30 +0900

    [add]jwt_utils.pyでトークンを生成・ユーザー情報を取得する

---
+++ authentication/views/jwt_utils.py
@@ -0,0 +1,32 @@
+import jwt
+import datetime
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+# 環境変数を読み込む
+env = environ.Env()
+env.read_env()
+
+User = get_user_model()
+
+# JWTトークンを生成する
+def generate_jwt(user):
+    payload = {
+        "user_id": user.id,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=JWT_ACCESS_TOKEN_EXPIRES),
+        "iat": datetime.datetime.utcnow(),
+    }
+    token = jwt.encode(payload, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+    return token
+
+# JWTトークンをデコードし、ユーザー情報を取得する
+def decode_jwt(token):
+    try:
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
+        return User.objects.get(id=payload["user_id"])
+    except jwt.ExpiredSignatureError:
+        return {'error': 'トークンの有効期限が切れています。'}
+    except jwt.InvalidTokenError:
+        return {'error': '無効なトークンです。'}
+    except User.DoesNotExist:
+        return {'error': 'ユーザーが存在しません。'}
