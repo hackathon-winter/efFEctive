c9f6b881b84dbf28a308478d2dee08381d5f7427
ARISA1115 <a.engine0808@gmail.com>
2025-02-09 20:00:09 +0900

    [fix]シングルクォーテーションに統一

--- authentication/models.py
+++ authentication/models.py
@@ -9,8 +9,14 @@ class CustomUserManager(BaseUserManager):
         email = self.normalize_email(email)
         extra_fields.setdefault('is_staff', False)  # デフォルトで is_staff=False
         extra_fields.setdefault('is_superuser', False)  # デフォルトで is_superuser=False
+        extra_fields.setdefault('google_authenticated', False) #Google認証ユーザーを区別する
+        
         user = self.model(email=email, **extra_fields)
-        user.set_password(password)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password() #Googleログインの場合、PW設定不要
+
         user.save(using=self._db)
         return user
 
@@ -19,21 +25,23 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)  # スーパーユーザーは is_superuser=True
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError("スーパーユーザーは is_staff=True である必要があります")
+            raise ValueError('スーパーユーザーは is_staff=True である必要があります')
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError("スーパーユーザーは is_superuser=True である必要があります")
+            raise ValueError('スーパーユーザーは is_superuser=True である必要があります')
 
         return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.AutoField(primary_key=True)  # ユーザーID（自動採番）
-    email = models.EmailField(unique=True, verbose_name="メールアドレス")
-    user_name = models.CharField(max_length=50, verbose_name="ユーザー名")
-    is_staff = models.BooleanField(default=False, verbose_name="スタッフ権限")  # 必須
-    is_active = models.BooleanField(default=True, verbose_name="アクティブ状態")  # 必須
-    created_at = models.DateTimeField(default=now, verbose_name="作成日時")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")
+    email = models.EmailField(unique=True, verbose_name='メールアドレス')
+    password_hash = models.CharField(max_length=255, verbose_name='パスワードハッシュ')
+    user_name = models.CharField(max_length=50, verbose_name='ユーザー名')
+    google_authenticated = models.BooleanField(default=False, verbose_name='Google認証ユーザー')
+    is_staff = models.BooleanField(default=False, verbose_name='スタッフ権限')  # 必須
+    is_active = models.BooleanField(default=True, verbose_name='アクティブ状態')  # 必須
+    created_at = models.DateTimeField(default=now, verbose_name='作成日時')
+    updated_at = models.DateTimeField(default=now, verbose_name='更新日時')
 
     objects = CustomUserManager()
 
@@ -41,5 +49,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['user_name']  # スーパーユーザー作成時に必須フィールド
 
     class Meta:
-        verbose_name = "ユーザー"
-        verbose_name_plural = "ユーザー一覧"
+        verbose_name = 'ユーザー'
+        verbose_name_plural = 'ユーザー一覧'
+
+    def__str__(self):
+        return self.email
