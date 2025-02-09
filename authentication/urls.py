1cb58f07879ba96720edaa05775d85818e3aa043
ARISA1115 <a.engine0808@gmail.com>
2025-02-10 00:14:38 +0900

    [fix]ルートを修正

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from authentication.views.auth import login_view, logout_view
+from authentication.views.register import register_view
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    #path('logout/', views.logout_view, name='logout'),
-    #path('register/', views.register_view, name='register'),
-    #path('profile/',views.profile_view, name='profile'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 ]
