2b62b5695ad8688f8fc92fc228338a8486be3970
ARISA1115 <a.engine0808@gmail.com>
2025-02-13 02:31:52 +0900

    [add]Googleログイン用URLパターンを追加

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from authentication.views.auth import login_view, logout_view
-from authentication.views.register import register_view
+from authentication.auth_views import login_view, logout_view
+from authentication.google_auth_views import google_login
+from authentication.register_views import register_view
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('google-login/', google_login, name='google-login'),
     path('register/', register_view, name='register'),
 ]
