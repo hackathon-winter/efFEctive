2364896bacb3a09976f32652a896719aefc3f420
root <root@DESKTOP-FIEJLUR>
2025-02-14 23:03:05 +0900

    問題表示、回答機能作成

--- questions/urls.py
+++ questions/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('list/', views.list_questions, name='list_questions'),
+    path('questions/', views.list_questions, name='list_questions'),
+    path('questions/<int:question_id>/answer/', views.answer_save, name='answer_save'),
     #path('detail/<int:question_id>', views.question_detail, name='question_detail'),
     #path('submit/', views.submit_answer, name='submit_answer'),
 ]
