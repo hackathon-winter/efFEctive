fc4e11a5d713b93c19f6d6ab1519cfc999ae3b21
ARISA1115 <a.engine0808@gmail.com>
2025-02-15 03:28:51 +0900

    [fix]urlパターンを修正

--- progress/urls.py
+++ progress/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('view/', views.view_progress, name='view_progress'),
-    #path('reset/', views.reset_progress, name='reset_progress'),
-    #path('update/', views.update_progress, name='update_progress'),
+    path('progress/', views.view_progress, name='view_progress'),
 ]
