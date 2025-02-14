29d1431ac24339cc6de90d42d28030d7ee55ad6c
ARISA1115 <a.engine0808@gmail.com>
2025-02-15 03:22:06 +0900

    [add]session_endを追加

--- progress/models.py
+++ progress/models.py
@@ -6,10 +6,12 @@ class Session(models.Model):
     user = models.ForeignKey('authentication.User', on_delete=models.CASCADE, related_name="sessions", verbose_name="ユーザー")
     correct_answers = models.IntegerField(verbose_name="正答数")
     total_questions = models.IntegerField(verbose_name="総問題数")
+    progress_percentage = models.FloatField(verbose_name='進捗率')
     start_time = models.DateTimeField(null=True, verbose_name="セッション開始日時")
     end_time = models.DateTimeField(null=True, verbose_name="セッション終了日時")
     created_at = models.DateTimeField(default=now, verbose_name="作成日時")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")
+    session_end = models.BooleanField(verbose_name='セッションが終了しているか')
 
     @property
     def progress_percentage(self):
