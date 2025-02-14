7318068bc8a0ce3e707bfceffd5f0e20c7c461c1
ARISA1115 <a.engine0808@gmail.com>
2025-02-15 03:29:43 +0900

    [fix]タイポ修正

--- authentication/admin.py
+++ authentication/admin.py
@@ -3,7 +3,7 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_diaplay = ('id', 'email', 'user_name', 'is_staff', 'is_active', 'created_at', 'updated_at')
+    list_display = ('id', 'email', 'user_name', 'is_staff', 'is_active', 'created_at', 'updated_at')
     list_filter = ('is_staff', 'is_active', 'created_at')
     search_fields = ('email', 'user_name')
     ordering = ('-created_at',)
