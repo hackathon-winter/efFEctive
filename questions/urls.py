f7dba4492ed0e62572cd825a9f410947eb8c7dd3
ARISA1115 <a.engine0808@gmail.com>
2025-02-17 00:21:14 +0900

    [fix]URLパターンを修正

--- questions/urls.py
+++ questions/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('questions/', views.list_questions, name='list_questions'),
+    path('questions/<int:question_id>', views.question_detail, name='question_detail'),
     path('questions/<int:question_id>/answer/', views.answer_save, name='answer_save'),
-    #path('detail/<int:question_id>', views.question_detail, name='question_detail'),
-    #path('submit/', views.submit_answer, name='submit_answer'),
+    path('questions/submit/', views.submit_answer, name='submit_answer'),
 ]
