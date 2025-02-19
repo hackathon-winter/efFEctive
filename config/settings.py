10660e58866f0d4658c1038e0104bbf9e315ddd1
hana222444 <hanar6stake@gmail.com>
2025-02-19 22:56:26 +0900

    uwsgi,nginxの導入

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = BASE_DIR / 'staticfiles' # DEBUG=False(本番環境)の場合に使用する
 
 # Default primary key field type
