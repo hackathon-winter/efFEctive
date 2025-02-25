b03b8b05617cc6ee684c9ed7b88ec8f3c1294ff6
ARISA1115 <a.engine0808@gmail.com>
2025-02-25 23:12:31 +0900

    [add]Djangoサーバー起動時にsignal.pyを読み込む

--- questions/apps.py
+++ questions/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class QuestionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'questions'
+
+    # Djangoサーバー起動時にsignal.pyを読み込む
+    def ready(self):
+        import questions.signals
