f2b3330de5a87fd217a626e9c0422dc3b8c8493c
ARISA1115 <a.engine0808@gmail.com>
2025-02-25 22:54:46 +0900

    [add]display_users メソッドを追加

--- rewards/admin.py
+++ rewards/admin.py
@@ -3,7 +3,11 @@ from .models import Badge
 
 @admin.register(Badge)
 class BadgeAdmin(admin.ModelAdmin):
-    list_display = ('user', 'category', 'level', 'acquired_at')
+    list_display = ('display_users', 'category', 'level', 'acquired_at')
     list_filter = ('category', 'level')
     search_fields = ('user__user_name', 'category')
     ordering = ('-acquired_at',)
+
+    def display_users(self, obj):
+        return ", ".join([user.email for user in obj.users.all()])
+    display_users.short_description = '取得ユーザー'
