561dc0ed4f63b55a9632eeab262cfde16cc2377b
ARISA1115 <a.engine0808@gmail.com>
2025-02-26 21:49:40 +0900

    [fix]BadgeAwardedを独立したモデルへ定義

--- rewards/models.py
+++ rewards/models.py
@@ -13,19 +13,19 @@ class Badge(models.Model):
         (GOLD, 'ゴールド'),
     ]
 
-    users = models.ManyToManyField(
-        'authentication.User',
-        blank=True,
-        verbose_name='取得ユーザー',
-        related_name='badges_awarded'
-    )
-
     category = models.CharField(max_length=50, choices=Question.CATEGORY_CHOICES, verbose_name='カテゴリ')
     level = models.CharField(max_length=10, choices=BADGE_LEVELS, verbose_name='ランク')
-    acquired_at = models.DateTimeField(auto_now_add=True, verbose_name='獲得日時')
-
-    class Meta:
-        unique_together = ('category', 'level')
 
     def __str__(self):
         return f'{self.get_category_display()}({self.get_level_display()})'
+
+class BadgesAwarded(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='badge_awards')
+    badge = models.ForeignKey(Badge, on_delete=models.CASCADE, related_name='badge_users')
+    acquired_at = models.DateTimeField(auto_now_add=True, verbose_name='獲得日時')
+
+    class Meta:
+        unique_together = ('user', 'badge')
+
+    def __str__(self):
+        return f'{self.user.user_name} - {self.badge}'
