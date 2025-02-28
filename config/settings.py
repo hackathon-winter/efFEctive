27f504026d27829cc584f0a3ff38cc01094d52b4
ARISA1115 <a.engine0808@gmail.com>
2025-02-28 21:25:01 +0900

    [fix]STATIC_URL = '/static/'へ修正

--- config/settings.py
+++ config/settings.py
@@ -121,9 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = BASE_DIR / 'staticfiles' # DEBUG=False(本番環境)の場合に使用する
+STATIC_URL = '/static/'
+
+if DEBUG:
+    STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+    STATIC_ROOT = None
+else: 
+    # DEBUG=False(本番環境)の場合に使用する
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
