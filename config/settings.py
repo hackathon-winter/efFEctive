afe7d4e0749b47e2a94157d35a77ff119973df97
ARISA1115 <a.engine0808@gmail.com>
2025-03-01 20:26:25 +0900

    [fix]STATICFILES_DIRS

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else: 
     # DEBUG=False(本番環境)の場合に使用する
-    STATICFILES_DIRS = []
+    STATICFILES_DIRS = [STATIC_ROOT]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
